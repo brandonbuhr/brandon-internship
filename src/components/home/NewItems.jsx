e679c60f97cc4641c0e38fccdc9296933ad9a8d0
brandonbuhr <brandonbuhr35@gmail.com>
2024-08-17 20:59:17 -0500

    fixed links to id and authorId

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -105,7 +105,7 @@ const NewItems = () => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
-                        to="/author"
+                        to={`/author/${item.authorId}`}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
                         title={`Creator: ${item.authorName}`}
@@ -145,7 +145,7 @@ const NewItems = () => {
                       </Link>
                     </div>
                     <div className="nft__item_info">
-                      <Link to="/item-details">
+                      <Link to={`/item-details/${item.id}`}>
                         <h4>{item.title}</h4>
                       </Link>
                       <div className="nft__item_price">{item.price} ETH</div>
