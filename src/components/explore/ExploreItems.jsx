3a88c9ba0a25b28f59536ca7b637ada39529134a
brandonbuhr <brandonbuhr35@gmail.com>
2024-08-17 22:47:30 -0500

    completed explore items section

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Countdown from "../FormatTime";
+
 const ExploreItems = ({ exploreData }) => {
+  const [visibleItems, setVisibleItems] = useState(8);
+  const [filter, setFilter] = useState("");
+
+  const handleLoadMore = () => {
+    setVisibleItems((prevVisibleItems) => {
+      if (prevVisibleItems + 4 > exploreData.length) {
+        return exploreData.length;
+      }
+      return prevVisibleItems + 4;
+    });
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    let filteredData = [...exploreData];
+
+    if (filter === "price_low_to_high") {
+      filteredData.sort((a, b) => a.price - b.price);
+    } else if (filter === "price_high_to_low") {
+      filteredData.sort((a, b) => b.price - a.price);
+    } else if (filter === "likes_high_to_low") {
+      filteredData.sort((a, b) => b.likes - a.likes);
+    }
+
+    return filteredData;
+  };
+
+  const filteredData = getFilteredData();
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" value={filter} onChange={handleFilterChange}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreData.map((item, index) => (
+      {filteredData.slice(0, visibleItems).map((item, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -77,9 +110,15 @@ const ExploreItems = ({ exploreData }) => {
         </div>
       ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {visibleItems < filteredData.length && (
+          <button
+            onClick={handleLoadMore}
+            id="loadmore"
+            className="btn-main lead"
+          >
+            Load more
+          </button>
+        )}
       </div>
     </>
   );
